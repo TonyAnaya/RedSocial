ac9ae9f65bed42b87c8da0a7445a13b59ec5a3bb
TonyAnaya <jaanaya99@gmail.com>
2022-03-04 15:02:38 -0600

    [DELETE] Se eliminaron "console.log" innecesarios

--- React/backend/model/update.js
+++ React/backend/model/update.js
@@ -1,7 +1,6 @@
 const sql = require("../db/conexion")
 
 module.exports.update= async (update)=>{
-    console.log(update)
     let response = await sql.query(`UPDATE users SET name='${update.name}', last_name='${update.last_name}', foto='${update.foto}', fondo='${update.fondo}', ciudad='${update.ciudad}', pais='${update.pais}', link='${update.link}', git='${update.git}', descri='${update.descri}' WHERE email='${update.email}';`)
 
     return response[0]
